046297dcf7c6bff89413a167b90d416b6951ef98
omid-sar <mr.omid.sardari@gmail.com>
2023-08-23 17:41:46 -0400

    Second Commit

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
+
 
 # Load the OpenAI API key from  apikey.py file
 from apikey import apikey
@@ -27,11 +29,16 @@ script_template = PromptTemplate(
     input_variables=["title"],
     template="write me a YouTube video script based on this {title}",
 )
+# Memory
+memory = ConversationBufferMemory(input_key="topic", memory_key="chat_history")
+
 # LLMS
 llm = OpenAI(temperature=0.0)
-title_chain = LLMChain(llm=llm, prompt=title_template, output_key="title", verbose=True)
+title_chain = LLMChain(
+    llm=llm, prompt=title_template, output_key="title", memory=memory, verbose=True
+)
 script_chain = LLMChain(
-    llm=llm, prompt=script_template, output_key="script", verbose=True
+    llm=llm, prompt=script_template, output_key="script", memory=memory, verbose=True
 )
 sequential_chain = SequentialChain(
     chains=[title_chain, script_chain],
@@ -42,5 +49,9 @@ sequential_chain = SequentialChain(
 
 # Screen Output
 if prompt:
-    response = sequential_chain(prompt)
-    st.write(response)
+    response = sequential_chain({"topic": prompt})
+    st.write(response["title"])
+    st.write(response["script"])
+
+    with st.expander(" Chat History"):
+        st.info(memory.buffer)
