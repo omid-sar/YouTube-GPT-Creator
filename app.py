8f9b4af08ecca9ef2ae8275770509b95dbec1b6e
omid-sar <mr.omid.sardari@gmail.com>
2023-08-23 13:14:43 -0400

    First Commit

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 # Import the necessary packages
 import os
 import streamlit as st
-
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain
 
 # Load the OpenAI API key from  apikey.py file
 from apikey import apikey
@@ -13,3 +15,32 @@ os.environ["OPENAI_API_KEY"] = apikey
 # APP Framework
 st.title("🦜🔗 YouTube GPT Creator")
 prompt = st.text_input("Plug in your prompt here")
+
+
+# Prompt Templates
+title_template = PromptTemplate(
+    input_variables=["topic"],
+    template="write me a YouTube video title about {topic}",
+)
+
+script_template = PromptTemplate(
+    input_variables=["title"],
+    template="write me a YouTube video script based on this {title}",
+)
+# LLMS
+llm = OpenAI(temperature=0.0)
+title_chain = LLMChain(llm=llm, prompt=title_template, output_key="title", verbose=True)
+script_chain = LLMChain(
+    llm=llm, prompt=script_template, output_key="script", verbose=True
+)
+sequential_chain = SequentialChain(
+    chains=[title_chain, script_chain],
+    input_variables=["topic"],
+    output_variables=["title", "script"],
+    verbose=True,
+)
+
+# Screen Output
+if prompt:
+    response = sequential_chain(prompt)
+    st.write(response)
